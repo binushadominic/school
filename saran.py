47dc48b9b84a0922094cb94495328db286affc65
binushadominic <132549223+binushadominic@users.noreply.github.com>
2023-05-04 12:53:47 +0530

    Add files via upload

---
+++ saran.py
@@ -0,0 +1,10 @@
+##count=0
+##while(count<9)
+##    print('The Count is:',count)
+##    count +=1
+##    print("Good bye!")i=1
+while i < 6:
+    print(i)
+    i+=1
+else:
+    print("i is no longer less than 6")
